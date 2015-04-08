15276bc838bd2d84ea4b92abe6e55b9ceb2eb0b0
Mary Bucknell <mbucknell@usgs.gov>
2015-04-08 12:38:37 -0500

    Initial project structure and set up of advanced page routing.

---
+++ src/main/webapp/js/util/templateLoader.js
@@ -0,0 +1,62 @@
+var GDP = GDP || {};
+
+GDP.util = GDP.util || {};
+
+GDP.util.templateLoader = function() {
+
+	var self = {};
+
+	var templates = {};
+
+	self.getTemplate = function(name) {
+		if (Object.has(templates, name)) {
+			return templates[name];
+		}
+		else {
+			return null;
+		}
+	};
+
+	self.loadTemplates = function(names) {
+		var i;
+		var loadingDeferreds = [];
+		for (i = 0; i < names.length; i++) {
+			templates[names[i]] = '';
+			loadingDeferreds.push($.ajax({
+				url : 'templates/' + names[i] + '.html',
+				success : function(data) {
+					templates[this] = Handlebars.compile(data);
+				},
+				error : function() {
+					templates[this] = Handlebars.compile('Unable to load template');
+				},
+				context : names[i]
+			}));
+		}
+
+		return $.when.apply(null, loadingDeferreds);
+	};
+
+	self.registerPartials = function(names) {
+		var i;
+		var loadingDeferreds = [];
+		for (i = 0; i < names.length; i++) {
+			loadingDeferreds.push($.ajax({
+				url : 'templates/partials/' + names[i] + '.html',
+				success : function(data) {
+					Handlebars.registerPartial(this, data);
+				},
+				error : function() {
+					Handlebars.registerPartial(this, 'Can\'t retrieve partial template');
+				},
+				context : names[i]
+			}));
+		}
+		return $.when.apply(null, loadingDeferreds);
+	};
+
+	return self;
+};
+
+
+
