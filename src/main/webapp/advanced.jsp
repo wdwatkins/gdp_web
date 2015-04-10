fc556466cda2592027d27d56a640d71e6c76c26b
Ivan Suftin <isuftin@usgs.gov>
2015-04-10 08:47:13 -0500

    Algorithm selection description

--- src/main/webapp/advanced.jsp
+++ src/main/webapp/advanced.jsp
@@ -1,6 +1,6 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
     <head>
 		<%@include file="/WEB-INF/jsp/head.jsp" %>
--- src/main/webapp/js/advanced/views/ProcessView.js
+++ src/main/webapp/js/advanced/views/ProcessView.js
@@ -5,8 +5,25 @@ var GDP = GDP || {};
 GDP.view = GDP.view || {};
 
 GDP.view.ProcessView = GDP.util.BaseView.extend({
+	events : {
+		"click .menu-dropdown-select-process" : "selectProcess"
+	},
+	
 	render : function () {
+		"use strict";
 		this.$el.html(this.template(this.collection.models));
 		return this;
+	},
+	
+	selectProcess : function (evt) {
+		"use strict";
+		var targetId = evt.target.id;
+		var processName = _.last(targetId.split('-'));
+		var $selectedDescription = $('#process-description-' +processName);
+		
+		// Remove all descriptions and show only the selected algorithm description
+		$('#container-process-description').children().not($selectedDescription).fadeOut(function () {
+			$selectedDescription.fadeIn();
+		});
 	}
 });
